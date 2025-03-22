3050dea8a9fd14d1d2f68d9aee207eaef030ba75
Nikola Milanovic <nikola.milanovic.dev@gmail.com>
2025-03-22 23:25:58 +0100

    Phone number dictionary exercise.

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 
-numbers = [5, 2, 7, 5, 8, 2, 11, 7, 46, 7, 5]
-new_array = []
+phone_nums = {
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine",
+    0: "Zero"
+}
 
-for number in numbers:
-    if number not in new_array:
-        new_array.append(number)
-print(new_array)
+phone = input("Phone: ")
+phone_number_in_words = ""
+for digit in phone:
+    phone_number_in_words += phone_nums[int(digit)]
+    phone_number_in_words += " "
+print(phone_number_in_words)
