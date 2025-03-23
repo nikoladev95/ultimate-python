85a714245943947fe551cd72746e8536df8d95c3
Nikola Milanovic <nikola.milanovic.dev@gmail.com>
2025-03-23 21:21:45 +0100

    Python class exercise with instancing.

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
 
-phone_nums = {
-    1: "One",
-    2: "Two",
-    3: "Three",
-    4: "Four",
-    5: "Five",
-    6: "Six",
-    7: "Seven",
-    8: "Eight",
-    9: "Nine",
-    0: "Zero"
-}
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def talk(self):
+        print(f"Hello, {self.name}")
 
-phone = input("Phone: ")
-phone_number_in_words = ""
-for digit in phone:
-    phone_number_in_words += phone_nums[int(digit)]
-    phone_number_in_words += " "
-print(phone_number_in_words)
+person1 = Person("Nikola")
+print(person1.name)
+person1.talk()
