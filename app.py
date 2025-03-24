ccd8ecf2f5cd727f437715b1ed03db249796cb9b
Nikola Milanovic <nikola.milanovic.dev@gmail.com>
2025-03-24 14:11:14 +0100

    Python automation with an Excel file.

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-#
-# from lucky_charm.dice import Dice
-#
-# dice_roll = Dice()
-# print(dice_roll.roll())
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
-from pathlib import Path
+def process_workbook(filename):
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
 
-path = Path()
-for file in path.glob('*.*'):
-    print(file)
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        corrected_price = cell.value * 0.9
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_price
+
+    values = Reference(sheet,
+                       min_row=2,
+                       max_row=sheet.max_row,
+                       min_col=4,
+                       max_col=4)
+
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, "e2")
+
+    wb.save(filename)
